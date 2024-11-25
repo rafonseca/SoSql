4cc311aba5d89331305ebef1998bd2d7cbd99e92
Renan A. Fonseca <renanfonseca@gmail.com>
2024-11-25 21:53:08 +0100

    [wip] implement soClause transformation

--- src/Lib.hs
+++ src/Lib.hs
@@ -36,6 +36,34 @@ transformSoSql :: Either String Ast.SoClause -> Either String PreparableStmt
 transformSoSql = fmap transformSoClause
 
 transformSoClause :: Ast.SoClause -> PreparableStmt
+transformSoClause soClause = let
+  Ast.SoClause ctes = soClause
+  with = WithClause False (fillEmptyFromClauses ctes)
+  asterisk = NormalTargeting (AsteriskTargetEl :| [])
+  from = RelationExprTableRef (SimpleRelationExpr (SimpleQualifiedName (QuotedIdent (pack "mystatement"))) False) Nothing Nothing
+  simpleselect = NormalSimpleSelect (Just asterisk) Nothing (Just (from:|[])) Nothing Nothing Nothing Nothing
+  select = SelectNoParens (Just with) (Left simpleselect) Nothing Nothing Nothing
+  in SelectPreparableStmt (Left select)
+   
+fillEmptyFromClauses ::  NonEmpty CommonTableExpr -> NonEmpty CommonTableExpr
+fillEmptyFromClauses cteExprs = let
+  ctesList = toList cteExprs
+  names = [name |CommonTableExpr name _ _ _ <- ctesList]
+  firstCte:subsequentCtes = ctesList
+  modifiedCtes = Prelude.zipWith replaceIdentInSelect names subsequentCtes
+  in
+  fromList (firstCte:modifiedCtes)
+
+
+replaceIdentInSelect :: Ident -> CommonTableExpr -> CommonTableExpr   
+replaceIdentInSelect newIdent (CommonTableExpr a b c (SelectPreparableStmt stmt)) = let
+  -- WIP
+  -- destructure stmt
+  newStmt = undefined
+  in
+  CommonTableExpr a b c (SelectPreparableStmt newStmt)
+replaceIdentInSelect _ cte = cte
+
 
 render ::  Either String PreparableStmt-> Text
 render (Left msg) = pack msg
